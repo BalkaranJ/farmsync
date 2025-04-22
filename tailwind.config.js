9d6ee9027b4997cf5709f7d1a00657314a7edff2
stvyn-namdy <webmetcreatives@gmail.com>
2025-04-21 20:55:09 -0600

    updated config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: {
-            DEFAULT: '#70c04e',
-            dark: '#5aa03c',
-          },
-          secondary: '#3d4852',
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#70c04e',
+          dark: '#5aa03c',
         },
-        fontFamily: {
-          sans: ['Inter', 'sans-serif'],
-        },
+        secondary: '#3d4852',
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
